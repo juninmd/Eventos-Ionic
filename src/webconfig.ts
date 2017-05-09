633334989db0c6798a1926e4aa4db858de5026f5
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2017-05-09 01:13:39 -0300

    url 'prod'

--- src/webconfig.ts
+++ src/webconfig.ts
@@ -2,5 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Webconfig {
-    UrlCore: string = "http://localhost:4500/";
+    UrlCore: string = "https://eventos99.herokuapp.com/";
+    //UrlCore: string = "http://localhost:4500/";
 }
